98d39ab55e9830245c65a6d927af193320aaaa1c
parkseyeonn <yondu0902@gmail.com>
2023-08-18 22:27:05 +0900

    page setting
    home, search, liked

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import "./globals.css";
+import { Figtree } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToastProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Spotify",
+  description: "Spotify",
+};
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const userSongs = await getSongsByUserId();
+
+  return (
+    <html lang='en'>
+      <body className={font.className}>
+        <ToastProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // ssr을 사용하므로 hydration 문제 발생할수도. ssr인경우 render하지 않음
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+    </>
+  );
+};
+
+export default ModalProvider;
