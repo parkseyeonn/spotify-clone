b74e68099d1c24ab4e4092f1c9f8d1c5cf9323e8
parkseyeonn <yondu0902@gmail.com>
2023-08-20 23:47:08 +0900

    error, loading

---
+++ app/search/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+  return (
+    <Box className='h-full flex justify-center items-center'>
+      <div className='text-neutral-400'>something went wrong.</div>
+    </Box>
+  );
+};
+
+export default Error;
